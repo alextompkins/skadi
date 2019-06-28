1d3d5458be16502ad905960270de652b0ae37513
Alex Tompkins <alextompkins3@gmail.com>
2019-06-28 12:46:31 +1200

    Add OpenCV Kotlin extensions courtesy of github.com/ramonrabello/OpenCV-Kotlin-Starter

---
+++ app/src/main/java/nz/atomic/skadi/extensions/BitmapExtensions.kt
@@ -0,0 +1,14 @@
+package nz.atomic.skadi.extensions
+
+import android.graphics.Bitmap
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+
+/**
+ * Extension functions for [Bitmap].
+ */
+fun Bitmap.toMat() : Mat {
+    val mat = Mat()
+    Utils.bitmapToMat(this, mat)
+    return mat
+}
