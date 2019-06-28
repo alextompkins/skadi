91e7188a9d678d91200ba6caade9cb9cf5cd49a6
Alex Tompkins <alextompkins3@gmail.com>
2019-06-28 12:18:29 +1200

    Add OpenCV library module for Android * Excluding .so native libs

--- app/src/main/java/nz/atomic/skadi/MainActivity.kt
+++ app/src/main/java/nz/atomic/skadi/MainActivity.kt
@@ -2,11 +2,30 @@ package nz.atomic.skadi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import org.opencv.android.InstallCallbackInterface
+import org.opencv.android.LoaderCallbackInterface
+import org.opencv.android.LoaderCallbackInterface.*
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, object : LoaderCallbackInterface {
+            override fun onManagerConnected(status: Int) {
+                when(status) {
+                    SUCCESS -> Toast.makeText(this@MainActivity,
+                        "OpenCV loaded successfully!", Toast.LENGTH_LONG).show()
+                    INIT_FAILED -> Toast.makeText(this@MainActivity,
+                        "OpenCV initialisation failed :(", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onPackageInstall(operation: Int, callback: InstallCallbackInterface?) {}
+        })
     }
+
 }
