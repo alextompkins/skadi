14de6af2a7d7273d913a4d85cc2fa1fec5176217
Alex Tompkins <alextompkins3@gmail.com>
2019-07-06 23:37:25 +1200

    Resolve memory leaks resulting from use of transient Mats without releasing them after * Change all extension functions on Mat to take a destination Mat as parameter * Modify TakePhotoActivity to be responsible for allocating/cleaning up its output frame

--- app/src/main/java/nz/atomic/skadi/TakePhotoActivity.kt
+++ app/src/main/java/nz/atomic/skadi/TakePhotoActivity.kt
@@ -9,11 +9,14 @@ import nz.atomic.skadi.extensions.resize
 import nz.atomic.skadi.extensions.transpose
 import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.JavaCameraView
+import org.opencv.core.CvType
 import org.opencv.core.Mat
 
 class TakePhotoActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
 
     private lateinit var cameraView: JavaCameraView
+
+    private var frameOut: Mat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +35,10 @@ class TakePhotoActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraView
     override fun onStop() {
         super.onStop()
         cameraView.disableView()
+
+        // Release frameOut matrix to free up memory
+        frameOut?.release()
+        frameOut = null
     }
 
     override fun onCameraViewStarted(width: Int, height: Int) {}
@@ -39,11 +46,19 @@ class TakePhotoActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraView
     override fun onCameraViewStopped() {}
 
     override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
-        val inputGray = inputFrame.gray()
-        return inputGray
-            .transpose()
-            .flip()
-            .canny(50.0, 100.0)
-            .resize(inputGray.size())
+        val frameIn = inputFrame.gray()
+        // Allocate frameOut if it has not yet been
+        if (frameOut == null) {
+            frameOut = Mat(frameIn.width(), frameIn.height(), CvType.CV_8U)
+        }
+
+        frameOut?.let {
+            frameIn
+                .transpose(it)
+                .flip(it)
+                .canny(it, 50.0, 100.0)
+                .resize(it, frameIn.size())
+        }
+        return frameOut!!
     }
 }
--- app/src/main/java/nz/atomic/skadi/extensions/MatExtensions.kt
+++ app/src/main/java/nz/atomic/skadi/extensions/MatExtensions.kt
@@ -8,64 +8,66 @@ import org.opencv.core.Mat
 import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
 
-/**
+/*
  * Extension functions for [Mat].
  */
-fun Mat.copy(): Mat {
-    val clone = Mat(height(), width(), type())
-    this.copyTo(clone)
-    return clone
-}
 
-fun Mat.toGray(): Mat {
-    val grayMat = Mat(height(), width(), CvType.CV_8U)
+fun Mat.toGray(grayMat: Mat): Mat {
     Imgproc.cvtColor(this, grayMat, Imgproc.COLOR_RGB2GRAY)
     return grayMat
 }
 
-fun Mat.transpose(): Mat {
-    val dstMat = Mat(width(), height(), type())
+fun Mat.transpose(dstMat: Mat): Mat {
     Core.transpose(this, dstMat)
     return dstMat
 }
 
-fun Mat.flip(): Mat {
-    val dstMat = copy()
+fun Mat.flip(dstMat: Mat): Mat {
     Core.flip(this, dstMat, 1)
     return dstMat
 }
 
-fun Mat.resize(size: Size): Mat {
-    val resized = Mat(size.width.toInt(), size.height.toInt(), this.type())
-    Imgproc.resize(this, resized, size)
-    return resized
+fun Mat.resize(dstMat: Mat, size: Size): Mat {
+    Imgproc.resize(this, dstMat, size)
+    return dstMat
 }
 
-fun Mat.gaussianBlur(kSize: Size = Size(125.toDouble(), 125.toDouble()), sigmaX:Double = 0.toDouble()): Mat {
-    val dstMat = copy()
+fun Mat.gaussianBlur(
+    dstMat: Mat,
+    kSize: Size = Size(125.toDouble(), 125.toDouble()),
+    sigmaX:Double = 0.toDouble()
+): Mat {
     Imgproc.GaussianBlur(this, dstMat, kSize, sigmaX)
     return dstMat
 }
 
-fun Mat.canny(threshold1: Double = 20.toDouble(), threshold2: Double = 255.toDouble()): Mat {
-    val srcMat = if (this.isGrayscale()) this else this.toGray()
-    val dstMat = srcMat.copy()
-    Imgproc.Canny(srcMat, dstMat, threshold1, threshold2)
+fun Mat.canny(
+    dstMat: Mat,
+    threshold1: Double = 20.toDouble(),
+    threshold2: Double = 255.toDouble()
+): Mat {
+    Imgproc.Canny(this, dstMat, threshold1, threshold2)
     return dstMat
 }
 
-fun Mat.threshold(thresh: Double = 50.toDouble(), maxVal: Double = 255.toDouble(), type:Int = Imgproc.THRESH_BINARY): Mat {
-    val srcMat = if (this.isGrayscale()) this else this.toGray()
-    val dstMat = srcMat.copy()
-    Imgproc.threshold(srcMat, dstMat, thresh, maxVal, type)
+fun Mat.threshold(
+    dstMat: Mat,
+    thresh: Double = 50.toDouble(),
+    maxVal: Double = 255.toDouble(),
+    type: Int = Imgproc.THRESH_BINARY
+): Mat {
+    Imgproc.threshold(this, dstMat, thresh, maxVal, type)
     return dstMat
 }
 
-fun Mat.adaptiveThreshold(maxValue: Double = 255.toDouble(), adaptiveMethod: Int = Imgproc.ADAPTIVE_THRESH_MEAN_C,
-                          thresholdType: Int = Imgproc.THRESH_BINARY, blockSize: Int = 11, c: Double = 12.toDouble()) : Mat {
-    val srcMat = if (this.isGrayscale()) this else this.toGray()
-    val dstMat = srcMat.copy()
-    Imgproc.adaptiveThreshold(srcMat, dstMat, maxValue, adaptiveMethod, thresholdType, blockSize, c)
+fun Mat.adaptiveThreshold(
+    dstMat: Mat,
+    maxValue: Double = 255.toDouble(),
+    adaptiveMethod: Int = Imgproc.ADAPTIVE_THRESH_MEAN_C,
+    thresholdType: Int = Imgproc.THRESH_BINARY, blockSize: Int = 11,
+    c: Double = 12.toDouble()
+) : Mat {
+    Imgproc.adaptiveThreshold(this, dstMat, maxValue, adaptiveMethod, thresholdType, blockSize, c)
     return dstMat
 }
 
