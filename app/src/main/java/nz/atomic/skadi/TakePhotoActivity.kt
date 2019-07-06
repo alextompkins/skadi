d13636f9a763f97250c05efb59408d2ba56ad9d5
Alex Tompkins <alextompkins3@gmail.com>
2019-07-07 00:27:43 +1200

    Implement much cleaner way to deal with allocating/releasing frameOut via overriding onCameraViewStarted/Stopped callbacks

--- app/src/main/java/nz/atomic/skadi/TakePhotoActivity.kt
+++ app/src/main/java/nz/atomic/skadi/TakePhotoActivity.kt
@@ -8,21 +8,20 @@ import nz.atomic.skadi.extensions.flip
 import nz.atomic.skadi.extensions.resize
 import nz.atomic.skadi.extensions.transpose
 import org.opencv.android.CameraBridgeViewBase
-import org.opencv.android.JavaCameraView
 import org.opencv.core.CvType
 import org.opencv.core.Mat
 
 class TakePhotoActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private lateinit var cameraView: JavaCameraView
-
-    private var frameOut: Mat? = null
+    private lateinit var cameraView: CameraBridgeViewBase
+    private lateinit var frameOut: Mat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_take_photo)
 
         cameraView = findViewById(R.id.cameraView)
+        cameraView.setMaxFrameSize(720, 480)
         cameraView.visibility = SurfaceView.VISIBLE
         cameraView.setCvCameraViewListener(this)
     }
@@ -35,30 +34,23 @@ class TakePhotoActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraView
     override fun onStop() {
         super.onStop()
         cameraView.disableView()
-
-        // Release frameOut matrix to free up memory
-        frameOut?.release()
-        frameOut = null
     }
 
-    override fun onCameraViewStarted(width: Int, height: Int) {}
+    override fun onCameraViewStarted(width: Int, height: Int) {
+        frameOut = Mat(width, height, CvType.CV_8U)
+    }
 
-    override fun onCameraViewStopped() {}
+    override fun onCameraViewStopped() {
+        frameOut.release()
+    }
 
     override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
         val frameIn = inputFrame.gray()
-        // Allocate frameOut if it has not yet been
-        if (frameOut == null) {
-            frameOut = Mat(frameIn.width(), frameIn.height(), CvType.CV_8U)
-        }
 
-        frameOut?.let {
-            frameIn
-                .transpose(it)
-                .flip(it)
-                .canny(it, 50.0, 100.0)
-                .resize(it, frameIn.size())
-        }
-        return frameOut!!
+        return frameIn
+            .transpose(frameOut)
+            .flip(frameOut)
+            .canny(frameOut, 50.0, 100.0)
+            .resize(frameOut, frameIn.size())
     }
 }
